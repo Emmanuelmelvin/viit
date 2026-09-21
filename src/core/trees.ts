32423205975b452f9a12a2174d14c12d752a24ef
Emmanuelmelvin <chidiemmanuel2005@gmail.com>
2026-09-21 12:07:03 +0100

    Add tree object reading

--- src/core/trees.ts
+++ src/core/trees.ts
@@ -1,4 +1,5 @@
 import { writeObject } from "./objects.js";
+import { readObject } from "./objects.js";
 
 type TreeNode = {
   files: Map<string, string>;
@@ -52,4 +53,39 @@ async function writeTreeNode(node: TreeNode): Promise<string> {
 
 export async function writeTree(index: Record<string, string>): Promise<string> {
   return writeTreeNode(buildTree(index));
+}
+
+export async function readTree(
+  treeId: string,
+  prefix = "",
+): Promise<Record<string, string>> {
+  const object = await readObject(treeId);
+
+  if (object.type !== "tree") {
+    throw new Error(`${treeId} is not a tree object`);
+  }
+
+  const entries: Record<string, string> = {};
+  let offset = 0;
+
+  while (offset < object.content.length) {
+    const space = object.content.indexOf(32, offset);
+    const nullByte = object.content.indexOf(0, space);
+    const mode = object.content.subarray(offset, space).toString();
+    const name = object.content.subarray(space + 1, nullByte).toString();
+    const objectId = object.content
+      .subarray(nullByte + 1, nullByte + 21)
+      .toString("hex");
+    const entryPath = prefix ? `${prefix}/${name}` : name;
+
+    if (mode.startsWith("4")) {
+      Object.assign(entries, await readTree(objectId, entryPath));
+    } else {
+      entries[entryPath] = objectId;
+    }
+
+    offset = nullByte + 21;
+  }
+
+  return entries;
 }
